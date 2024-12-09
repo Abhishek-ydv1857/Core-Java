076a37f56443dbddb7d24a44553a42a0576bf907
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-09 11:36:06 +0530

    Java Programs

---
+++ Fivefri.java
@@ -0,0 +1,9 @@
+public class Fivefri {
+    public static void main(String[] args) {
+        System.out.println("Prem bhaiya");
+        System.out.println("Jeveen bhai");
+        System.out.println("Arun bhai");
+        // System.out.println("Nikhil bhai");
+        // System.out.println("Summit bhai");
+    }
+}
