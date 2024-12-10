0f07a3032cb47f842359bf364d541a0d356053b6
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-10 16:25:48 +0530

    uploading file

---
+++ Basic/basic-operations/A11.java
@@ -0,0 +1,12 @@
+public class A11 {
+    public static void main(String[] args) {
+        float a=1.6f;
+        float b=12f;
+        float c=2f;
+        float d=2f;
+        float e=7.8f;
+        float f=1.5f;
+        float slove =a*(b-c)/d+e-f; 
+        System.out.println(slove);
+    }
+}
