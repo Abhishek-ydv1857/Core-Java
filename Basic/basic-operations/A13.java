8425d3d2ca859f73ac301dec74057f6af0c0f939
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-10 16:38:26 +0530

    uploading file

---
+++ Basic/basic-operations/A13.java
@@ -0,0 +1,12 @@
+public class A13 {
+    public static void main(String[] args) {
+        float a=8f;
+        float b=1f;
+        float c=4f;
+        float d=2f;
+        float e=1f;
+        float f=6f;
+        float slove =a*b+(c-d)/e+f; 
+        System.out.println(slove);
+    }
+}
