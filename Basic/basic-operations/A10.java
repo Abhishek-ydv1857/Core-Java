386214a76df7a12bc35cdf744e651b734025cc64
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-10 16:13:38 +0530

    uploading file

---
+++ Basic/basic-operations/A10.java
@@ -0,0 +1,11 @@
+public class A10 {
+    public static void main(String[] args) {
+        int a=56;
+        int b=34;
+        int c=12;
+        int d=2;
+        int e=13;
+        float sum=a*34+c/d-e;
+        System.out.println(sum);
+    }
+}
