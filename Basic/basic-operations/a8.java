324673b913b9854019014ac5e5dd7f4bae5b6854
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-10 16:05:41 +0530

    uploadin a8 file

---
+++ Basic/basic-operations/a8.java
@@ -0,0 +1,11 @@
+public class a8 {
+    public static void main(String[] args) {
+        int a=34;
+        int b=67;
+        int c=23;
+        int d=12;
+        int e=6;
+        float sum=a+b+c+d+e;
+        System.out.println(sum);
+    }
+}
