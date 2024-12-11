3ebee2ba1f4aeab8f274f38bd5111b0f73c2fe35
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-11 16:48:36 +0530

    uploading file

---
+++ Basic/basic-operations/A21.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class A21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter value of a :");
+        int a = sc.nextInt();
+        System.out.println("enter value of b :");
+        int b = sc.nextInt(); 
+        int c ;
+        c=a-b;
+        System.out.println(c);
+    }
+}
---
+++ Basic/basic-operations/A24.java
@@ -0,0 +1,18 @@
+
+
+import java.util.Scanner;
+
+public class A24
+ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int e = sc.nextInt();
+        int slove;
+        slove=a+b+c+d+e;
+        System.out.println(slove);
+    }
+}
