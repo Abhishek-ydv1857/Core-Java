004b3949b90a507fc746b559fac3d508eb01bb81
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-12 14:27:22 +0530

    Uploading file

---
+++ Basic/Control-statements/A2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class A2 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number :");
+        int a = sc.nextInt();
+        if(a>10){
+            System.out.println("a is greater than 10");
+        }
+        else {
+            System.out.println("a is not greater than 10");
+        }
+
+    }
+}
---
+++ Basic/Control-statements/A5.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class A5 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your numbers :");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int num4 = sc.nextInt();
+        int num5 = sc.nextInt();
+        if(num1>num2 && num1>num3 &&){
+            System.out.println("num1 is greater");
+        }
+        else if (num2>num1&&num2>num3){
+            System.out.println("num2 is greater");
+        }
+        else{
+            System.out.println("num3 is greater");
+        }
+
+    }
+}
