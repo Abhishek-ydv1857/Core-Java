aff9ced40432291b6aaf51a348ad4ddb554c2241
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-12 10:53:21 +0530

    Uploading file

---
+++ Basic/basic-operations/A30.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class A28
+ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float c = sc.nextFloat();
+        float d = sc.nextFloat();
+        float e = sc.nextFloat();
+        float f = sc.nextFloat();
+        float g = sc.nextFloat();
+        float slove;
+        slove=a*b-(c-d)+e-f +g;
+        System.out.println(slove);
+    }
+}
