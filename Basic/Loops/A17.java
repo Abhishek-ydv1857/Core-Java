6bc91bac1afd2035605c397b19800c50fc663095
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-17 11:31:17 +0530

    Uploading file

--- Basic/Loops/A17.java
+++ Basic/Loops/A17.java
@@ -2,6 +2,12 @@ import java.util.*;
 public class A17 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        
+        int i =1;
+        while(i<=100){
+            if(i%3==0 || i%7==0){
+                System.out.println(i);
+            }
+            i++;
+        }
     }
 }
---
+++ Basic/Loops/A18.java
@@ -0,0 +1,11 @@
+public class A18 {
+    public static void main(String[] args) {
+        int i = 10;
+
+        System.out.println("Table of 2 in reverse order:");
+        while (i >= 1) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+            i--;
+        }
+    }
+}
