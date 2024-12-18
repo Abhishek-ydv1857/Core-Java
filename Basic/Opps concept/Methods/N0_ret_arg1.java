cbed507b5eeb5acf88dd5813e18d7bd8d3312177
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-18 14:11:48 +0530

    No return and no argument

---
+++ Basic/Opps concept/Methods/N0_ret_arg1.java
@@ -0,0 +1,11 @@
+class A{
+    void print(){
+        System.out.println("Hello World");
+    }
+}
+public class N0_ret_arg1 {
+    public static void main(String[] args) {
+        A p=new A();
+        p.print();
+    }
+}
