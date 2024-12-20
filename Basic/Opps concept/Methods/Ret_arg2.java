313ce0aa054517a1fa6a567b1e0a18f0a283e256
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-20 16:50:54 +0530

    Return and argument

---
+++ Basic/Opps concept/Methods/Ret_arg2.java
@@ -0,0 +1,11 @@
+class A{
+  String a(String s){
+    return s;
+  }  
+}
+public class Ret_arg2 {
+    public static void main(String[] args) {
+        A p = new A();
+        System.out.println(p.a("Abhishek"));
+    }
+}
