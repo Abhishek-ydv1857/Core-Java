c355992328196a55dd21e11b10213635681254ea
Abhishek <ayabhishek2003yadav@gmail.com>
2024-12-23 14:09:23 +0530

    Instance Variable

---
+++ Basic/Opps concept/Constructor/A3.java
@@ -0,0 +1,14 @@
+class A{
+    int a=2;
+    int b=3;
+    void add(){
+       int add = a+b;
+       System.out.println(add); 
+    }
+}
+public class A3 {
+    public static void main(String[] args) {
+        new A().add();
+        
+    }
+}
