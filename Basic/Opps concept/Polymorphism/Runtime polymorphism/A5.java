7ba7f168f5ce3bcd2a3a63098fa2800121bbd055
Abhishek <ayabhishek2003yadav@gmail.com>
2025-01-05 13:27:21 +0530

    Multilevel inheritance method overrided

---
+++ Basic/Opps concept/Polymorphism/Runtime polymorphism/A5.java
@@ -0,0 +1,23 @@
+class A{
+    void show(){
+        System.out.println("A class ");
+    }
+}
+class B extends A{
+    void show(){
+        super.show();
+        System.out.println("B class");
+    }
+}
+class C extends B{
+    void show(){
+        super.show();
+        System.out.println("C class");
+    }
+}
+public class A5 {
+    public static void main(String[] args) {
+        C p=new C();
+        p.show();
+    }
+}
