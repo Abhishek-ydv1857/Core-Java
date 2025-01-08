e4c3662c14985d71350a201425024b004941a442
Abhishek <ayabhishek2003yadav@gmail.com>
2025-01-08 20:14:02 +0530

    Polymorphism

---
+++ Basic/Opps concept/Polymorphism/B3.java
@@ -0,0 +1,35 @@
+
+// single level inheritance : 
+// method :
+// first method :public 
+// second method :protected 
+
+public class B3 
+{
+   
+
+   public static void main(String[] args) 
+   {
+       B b=new B();
+       b.method1();
+       b.method2();
+   }
+}
+class A
+{
+        public void method1()
+        {
+           System.out.println("method 1");
+        }
+
+
+}
+       
+class B extends A
+{
+   protected void method2()
+   {
+      System.out.println("method 2");
+   }
+}
+
