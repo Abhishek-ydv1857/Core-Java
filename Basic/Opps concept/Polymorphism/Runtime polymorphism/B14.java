e5e869a824433762f760e7d49d91cd990b7228fd
Abhishek <ayabhishek2003yadav@gmail.com>
2025-01-09 11:48:43 +0530

    single iheritance inheritance

---
+++ Basic/Opps concept/Polymorphism/Runtime polymorphism/B14.java
@@ -0,0 +1,29 @@
+
+
+public class B14 
+{   
+
+    public static void main(String[] args) 
+	{
+        B b=new B();
+        b.method();
+        
+    }
+}
+class A
+{
+                protected void method()
+                {
+                  System.out.println("method 1");  
+                }
+}
+class B extends A
+{
+      protected void method()
+      {
+            super.method();
+            System.out.println("method 2");
+      }
+
+}
+
