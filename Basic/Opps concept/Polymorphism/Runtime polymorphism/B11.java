a3eb907a2b8251bdfc23ad80eef1c2df68b73ad4
Abhishek <ayabhishek2003yadav@gmail.com>
2025-01-09 11:42:46 +0530

    Multilevel inheritance

---
+++ Basic/Opps concept/Polymorphism/Runtime polymorphism/B11.java
@@ -0,0 +1,45 @@
+
+public class B11
+{
+    
+    public static void main(String[] args) 
+	{
+      C  c=new C();
+      c.method3();
+      c.another();
+       
+    }
+}
+class A
+{
+    private void method1()
+    {
+       System.out.println("method 1");
+    }
+    void another()
+    {
+        method1();
+    }
+}
+        
+class B extends A
+{
+    
+    
+    public void method2()
+    {
+       
+       System.out.println("method 2");
+    }
+   
+
+
+}
+class C extends B
+{
+     void method3()
+     {
+       System.out.println("method 3");
+     }
+}
+
