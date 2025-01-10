390ebd3730e497ac6cda30ff63918759c5d186fd
Abhishek <ayabhishek2003yadav@gmail.com>
2025-01-10 14:24:13 +0530

    Abstract class and methods

---
+++ Basic/Opps concept/Abstraction/A3.java
@@ -0,0 +1,30 @@
+
+public class A3 
+{
+    public static void main(String[] args) 
+	{
+        B b=new B();
+        b.show();
+    }
+}
+abstract class A
+{
+    A()
+    {
+        System.out.println("Construcuture call");
+    }
+
+    abstract void show();
+}
+class B extends A
+{
+    B()
+    {
+        super();
+    }
+    void show()
+    {
+        System.out.println("Void method");
+    }
+}
+
