c8f58b18bbe2ed98c144ef3b2ef0511f25b8c8d0
Abhishek <ayabhishek2003yadav@gmail.com>
2025-02-10 12:51:45 +0530

    array first & last index

---
+++ Basic/array/A6.java
@@ -0,0 +1,9 @@
+public class A6 {
+    public static void main(String[] args) {
+        int arr[] = {12,67,46,435,67,385,335,335,34};
+        int first=arr[0];
+        System.out.println("First " +first);
+        int last=arr.length-1;
+        System.out.println("last " + arr[last]);
+    }
+}
