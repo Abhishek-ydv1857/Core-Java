d425059ed1fd20a32ad55a319ffabc84e8e4d0f2
Abhishek <ayabhishek2003yadav@gmail.com>
2025-02-10 12:41:49 +0530

    print mid element of array

---
+++ Basic/array/A5.java
@@ -0,0 +1,8 @@
+public class A5 {
+    public static void main(String[] args) {
+        int arr[]={12,67,46,435,67,385,335,335,34};
+        int midarray = arr.length/2;
+        System.out.println(arr[midarray]);
+        }
+    }
+
