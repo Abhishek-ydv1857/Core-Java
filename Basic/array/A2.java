c344721c86cb652121353e38808d8cbcee7b6087
Abhishek <ayabhishek2003yadav@gmail.com>
2025-02-10 12:22:43 +0530

    Print array

---
+++ Basic/array/A2.java
@@ -0,0 +1,26 @@
+
+// String :five names 
+// float type :
+
+public class A2 {
+    public static void main(String[] args) {
+        char ch[]={'a','b','c','d'};
+        String str[]={"abhi","nirmal","rahul","kamal"};
+        float fl[]={34.5f,34.5f,55f,98.5f};
+        //char print
+        for(int i=0;i<ch.length;i++){
+            System.out.print(ch[i] + " ");
+        }
+            System.out.println();
+        //String print 
+        for(int j=0;j<str.length;j++){
+            System.out.print(str[j] + " ");
+        } 
+        System.out.println();
+        //float print
+        for(int k=0;k<fl.length;k++){
+            System.out.print(fl[k] +" ");
+        }
+        
+    }
+}
