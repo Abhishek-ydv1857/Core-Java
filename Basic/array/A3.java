fd2c398ee8fd04638fc28c0d5cb35c3ba225f91d
Abhishek <ayabhishek2003yadav@gmail.com>
2025-02-10 12:26:26 +0530

    print array forward direction

---
+++ Basic/array/A3.java
@@ -0,0 +1,8 @@
+public class A3 {
+    public static void main(String []args){
+        int arr[]={12,67,46,435,67,385,335,335,34};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] +" ");
+        }
+    }
+}
