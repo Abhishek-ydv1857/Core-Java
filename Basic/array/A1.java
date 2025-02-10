78bbfe6441af829e6d0a250e31ae0dc9af9f6611
Abhishek <ayabhishek2003yadav@gmail.com>
2025-02-10 12:02:01 +0530

    Length of array

---
+++ Basic/array/A1.java
@@ -0,0 +1,6 @@
+public class A1 {
+    public static void main(String[] args) {
+        int[] numbers = {12, 67, 46, 435, 67, 385, 335, 335, 34};
+        System.out.println("Length of the array: " + numbers.length);
+    }
+}
