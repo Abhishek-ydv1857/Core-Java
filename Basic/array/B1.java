2c8c88f3a6e5eacef30da68467444cad902a3f8f
Abhishek <ayabhishek2003yadav@gmail.com>
2025-02-19 12:42:41 +0530

    User input array

--- Basic/array/B1.java
+++ Basic/array/B1.java
@@ -1,24 +1,22 @@
+
 import java.util.Scanner;
 
-public class B1 {
+public class B1{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your array size : ");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
 
-        System.out.print("Enter array size: ");
-        int size = scanner.nextInt();
-
-        int[] numbers = new int[size];
-
-        System.out.println("Enter " + size + " elements:");
-        for (int i = 0; i < size; i++) {
-            numbers[i] = scanner.nextInt();
+        System.out.println("enter your array : ");
+        for(int i=0;i<arr.length;i++){
+             arr[i]=sc.nextInt();
+            
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
         }
 
-        System.out.print("Array elements: ");
-        for (int num : numbers) {
-            System.out.print(num + " ");
-        }
 
-        scanner.close();
     }
 }
